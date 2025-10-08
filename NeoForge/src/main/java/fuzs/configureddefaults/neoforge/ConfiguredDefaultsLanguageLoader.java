8808678ebe5ce68136ba5c9e6a5b654913602829
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-10-08 10:29:22 +0200

    prepare 1.21.10 port

---
+++ NeoForge/src/main/java/fuzs/configureddefaults/neoforge/ConfiguredDefaultsLanguageLoader.java
@@ -0,0 +1,29 @@
+package fuzs.configureddefaults.neoforge;
+
+import fuzs.configureddefaults.ConfiguredDefaults;
+import fuzs.configureddefaults.handler.CopyDefaultsHandler;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModLoadingException;
+import net.neoforged.fml.loading.BuiltInLanguageLoader;
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforgespi.language.IModInfo;
+import net.neoforged.neoforgespi.language.ModFileScanData;
+
+public class ConfiguredDefaultsLanguageLoader extends BuiltInLanguageLoader {
+
+    public ConfiguredDefaultsLanguageLoader() {
+        // with Architectury Loom NeoForge is unable to access the common module, so copy everything here
+        CopyDefaultsHandler.initialize(FMLLoader.getCurrent().getGameDir(), FMLEnvironment.getDist().isClient());
+    }
+
+    @Override
+    public String name() {
+        return ConfiguredDefaults.MOD_NAME;
+    }
+
+    @Override
+    public ModContainer loadMod(IModInfo info, ModFileScanData modFileScanResults, ModuleLayer layer) throws ModLoadingException {
+        return null;
+    }
+}
